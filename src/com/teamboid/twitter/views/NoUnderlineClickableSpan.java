2cf72d14cf2bf75a00c9207aa8c815ee7205c89d
Zach Rogers <zachrog@gmail.com>
2012-07-15 04:30:41 -0700

    Readded bug fix with blank column after theme changes

--- src/com/teamboid/twitter/views/NoUnderlineClickableSpan.java
+++ src/com/teamboid/twitter/views/NoUnderlineClickableSpan.java
@@ -6,12 +6,10 @@ import android.text.style.ClickableSpan;
 /**
  * @author Aidan Follestad
  */
-public abstract class NoUnderlineClickableSpan extends ClickableSpan
-{
-
+public abstract class NoUnderlineClickableSpan extends ClickableSpan {
+	
 	@Override
-	public void updateDrawState(TextPaint ds)
-	{
+	public void updateDrawState(TextPaint ds) {
 		ds.setColor(ds.linkColor);
 		ds.setUnderlineText(false);
 	}
