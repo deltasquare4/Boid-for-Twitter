c9110f6a7818b99d3446f2fe24d02b1de35762d8
Joe Simpson <headbangerkenny@googlemail.com>
2012-07-21 20:00:25 +0100

    Night Mode
    - Actually effective
    - Bug fixes in AccountManger
    - Push is effected

---
+++ src/com/teamboid/twitter/utilities/NightModeUtils.java
@@ -0,0 +1,39 @@
+package com.teamboid.twitter.utilities;
+
+import java.util.Calendar;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class NightModeUtils {
+	public static Calendar parseTimePreference(String input){
+		Calendar c = Calendar.getInstance();
+		String[] parts = input.split(":");
+		c.set(Calendar.HOUR, Integer.parseInt( parts[0] ));
+		c.set(Calendar.MINUTE, Integer.parseInt( parts[1] ));
+		return c;
+	}
+	
+	/**
+	 * Is it nightime yet? I'm tired!
+	 * @param c
+	 * @return
+	 */
+	public static boolean isNightMode(Context c){
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(c);
+		
+		if( prefs.getBoolean("night_mode", false) == false ) return false; // SLEEP IS FOR THE WEAK!
+		
+		Calendar now = Calendar.getInstance();
+		Calendar time = parseTimePreference(prefs.getString("night_mode", "00:00"));
+		if(now.after(time)){ // We have passed the begining of the night time
+			time = parseTimePreference(prefs.getString("night_mode_endtime", "00:00"));
+			if(now.before(time)){ // We are actually inside night time now
+				return true;
+			}
+		}
+		
+		return false; // Did not meet all conditions
+	}
+}
