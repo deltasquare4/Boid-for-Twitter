abd9cd379189f2fc048d994c7055a31f97f9dc3f
Aidan Follestad <drummer.aidan@gmail.com>
2012-08-06 01:45:18 -0500

    Improved code readability in Api16.java

--- src/com/teamboid/twitter/compat/Api16.java
+++ src/com/teamboid/twitter/compat/Api16.java
@@ -3,7 +3,6 @@ package com.teamboid.twitter.compat;
 import com.handlerexploit.prime.ImageManager;
 import com.teamboid.twitter.ComposerScreen;
 import com.teamboid.twitter.R;
-import com.teamboid.twitter.TweetViewer;
 import com.teamboid.twitter.utilities.Utilities;
 
 import com.teamboid.twitterapi.dm.DirectMessage;
@@ -26,19 +25,22 @@ import android.graphics.Bitmap;
 @TargetApi(16)
 public class Api16 {
 	
+	private static PendingIntent getReplyActionIntent(Context context, Status s) {
+		Intent replyIntent = new Intent(context, ComposerScreen.class)
+    		.putExtra("reply_to", s.getId())
+    		.putExtra("reply_to_name", s.getUser().getScreenName())
+    		.putExtra("append", Utilities.getAllMentions(s))
+    		.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		PendingIntent replyPi = PendingIntent.getActivity(context, 0, replyIntent, PendingIntent.FLAG_ONE_SHOT);
+		return replyPi; 
+	}
+	
 	/**
 	 * Please call Api11.displayNotification instead. This gets called by it if we're on JB
 	 */
 	public static void displayReplyNotification(final int accId, final Context context, final Bitmap profileImg, final Status s, final Builder nb, final NotificationManager nm) {
-		
-		Intent replyIntent = new Intent(context, ComposerScreen.class)
-        	.putExtra("reply_to", s.getId())
-        	.putExtra("reply_to_name", s.getUser().getScreenName())
-        	.putExtra("append", Utilities.getAllMentions(s))
-        	.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		PendingIntent replyPi = PendingIntent.getActivity(context, 0, replyIntent, PendingIntent.FLAG_ONE_SHOT);
-		
-		nb.addAction(R.drawable.reply_light, context.getString(R.string.reply_str), replyPi);
+		nb.addAction(R.drawable.reply_light, context.getString(R.string.reply_str),
+				getReplyActionIntent(context, s));
 		nb.addAction(R.drawable.retweet_light, context.getString(R.string.retweet_str), null); //TODO
 		
 		String media = Utilities.getTweetYFrogTwitpicMedia(s);
