68d074b5af974314734067a361ac19a1dfc162a6
Aidan Follestad <drummer.aidan@gmail.com>
2012-08-06 01:05:54 -0500

    Added expandable notification actions, still need to make them functional (except reply, which works).

--- src/com/teamboid/twitter/compat/Api16.java
+++ src/com/teamboid/twitter/compat/Api16.java
@@ -1,6 +1,9 @@
 package com.teamboid.twitter.compat;
 
 import com.handlerexploit.prime.ImageManager;
+import com.teamboid.twitter.ComposerScreen;
+import com.teamboid.twitter.R;
+import com.teamboid.twitter.TweetViewer;
 import com.teamboid.twitter.utilities.Utilities;
 
 import com.teamboid.twitterapi.dm.DirectMessage;
@@ -8,9 +11,11 @@ import com.teamboid.twitterapi.status.Status;
 
 import android.annotation.TargetApi;
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Notification.Builder;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 
 /**
@@ -20,11 +25,22 @@ import android.graphics.Bitmap;
  */
 @TargetApi(16)
 public class Api16 {
-
+	
 	/**
 	 * Please call Api11.displayNotification instead. This gets called by it if we're on JB
 	 */
 	public static void displayReplyNotification(final int accId, final Context context, final Bitmap profileImg, final Status s, final Builder nb, final NotificationManager nm) {
+		
+		Intent replyIntent = new Intent(context, ComposerScreen.class)
+        	.putExtra("reply_to", s.getId())
+        	.putExtra("reply_to_name", s.getUser().getScreenName())
+        	.putExtra("append", Utilities.getAllMentions(s))
+        	.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		PendingIntent replyPi = PendingIntent.getActivity(context, 0, replyIntent, PendingIntent.FLAG_ONE_SHOT);
+		
+		nb.addAction(R.drawable.reply_light, context.getString(R.string.reply_str), replyPi);
+		nb.addAction(R.drawable.retweet_light, context.getString(R.string.retweet_str), null); //TODO
+		
 		String media = Utilities.getTweetYFrogTwitpicMedia(s);
 		if(media != null && !media.isEmpty()) {
 			ImageManager.getInstance(context).get(media, new ImageManager.OnImageReceivedListener() {
@@ -51,6 +67,9 @@ public class Api16 {
 	}
 
 	public static void displayDMNotification(final int accId, final Context context, final Bitmap profileImg, final DirectMessage msg, final Builder nb, final NotificationManager nm, final String text) {
+		nb.addAction(R.drawable.reply_light, context.getString(R.string.reply_str), null); //TODO
+		nb.addAction(R.drawable.delete_light, context.getString(R.string.delete_str), null); //TODO
+		
 		Notification noti = Api11.setupNotification(accId,
 				new Notification.BigTextStyle(nb)
 		.bigText(text)
