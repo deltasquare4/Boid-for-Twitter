84b6a013e3b361e0c9ab6ee4ff4d85b80fd311e7
Joe Simpson <headbangerkenny@googlemail.com>
2012-09-15 20:57:55 +0100

    Time.is widget + Account Prefs
    - Accounts now show + prefs work inside of the std settings screen
    - Danger zone system to remove account. Removal is not a normal operation (long press is not a good solution)
    - You can now preview http://time.is/4pm_in_UK and other URLs. It's something I think should be used a hell of a lot more :)

--- src/com/teamboid/twitter/SettingsScreen.java
+++ src/com/teamboid/twitter/SettingsScreen.java
@@ -5,24 +5,35 @@ import net.robotmedia.billing.BillingRequest.ResponseCode;
 import net.robotmedia.billing.helper.AbstractBillingObserver;
 import net.robotmedia.billing.model.Transaction.PurchaseState;
 
+import com.teamboid.twitter.contactsync.AndroidAccountHelper;
+import com.teamboid.twitter.notifications.NotificationService;
 import com.teamboid.twitter.services.AccountService;
 import com.teamboid.twitter.utilities.Utilities;
+import com.teamboid.twitter.views.AccountHeaderPreference;
 import com.teamboid.twitterapi.media.MediaServices;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import me.kennydude.awesomeprefs.Preference;
 import me.kennydude.awesomeprefs.Preference.OnPreferenceChangeListener;
 
+import me.kennydude.awesomeprefs.HeaderPreference;
 import me.kennydude.awesomeprefs.ListPreference;
 import me.kennydude.awesomeprefs.Preference.OnPreferenceClickListener;
 import me.kennydude.awesomeprefs.PreferenceActivity;
+import me.kennydude.awesomeprefs.PreferenceCategory;
 import me.kennydude.awesomeprefs.PreferenceFragment;
+import me.kennydude.awesomeprefs.SwitchPreference;
 
 import android.preference.PreferenceManager;
+import android.provider.ContactsContract;
 import android.provider.SearchRecentSuggestions;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.NumberPicker;
 import android.widget.TextView;
@@ -85,7 +96,31 @@ public class SettingsScreen extends PreferenceActivity  {
 		
 	@Override
 	public void finishAddingPreferences(){
-		// TODO: Add accounts
+		addAccounts();
+	}
+	
+	public void addAccounts(){
+		for(Preference p : this.getHeaderFragment().findPreferencesByClass("account")){
+			p.remove();
+		}
+		
+		if(AccountService.getAccounts().size() > 0){
+			PreferenceCategory accHeader = (PreferenceCategory) getHeaderFragment().findPreference("accounts");
+			
+			for(Account acc : AccountService.getAccounts()){
+				AccountHeaderPreference hp = new AccountHeaderPreference(this	, getHeaderFragment());
+				hp.setTitle(acc.getUser().getScreenName());
+				hp.url = Utilities.getUserImage(acc.getUser().getScreenName(), this, acc.getUser());
+				hp.Class = "account";
+				
+				Bundle extras = new Bundle();
+				extras.putString("accountName", acc.getUser().getScreenName());
+				extras.putLong("accountId", acc.getId());
+				hp.setFragment(AccountFragment.class, extras);
+				
+				accHeader.addPreference(hp);
+			}
+		}
 		
 		getHeaderFragment().findPreference("addNewAccount").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
 
@@ -129,6 +164,129 @@ public class SettingsScreen extends PreferenceActivity  {
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	public static class AccountFragment extends PreferenceFragment {
+
+		@Override
+		public void onDestroy() {
+			super.onDestroy();
+		}
+
+		long accountId;
+		SharedPreferences sp;
+		
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+
+			addPreferencesFromResource(R.xml.prefs_accounts);
+			
+			getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+			getActivity().setTitle(getArguments().getString("accName"));
+			accountId = this.getArguments().getLong("accountId");
+			sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+			setKey("c2dm", accountId);
+			setKey("c2dm_period", accountId);
+			setKey("c2dm_mention", accountId);
+			setKey("c2dm_dm", accountId);
+			setKey("c2dm_vibrate", accountId);
+			setKey("c2dm_ringtone", accountId);
+			setKey("contactsync_on", accountId);
+			
+			findPreference("delete").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+
+				@Override
+				public boolean onPreferenceClick(Preference pref) {
+					final Account acc = AccountService.getAccount(accountId);
+					AlertDialog.Builder ab = new AlertDialog.Builder(
+							getActivity())
+							.setTitle(R.string.remove_account)
+							.setMessage(
+									getString(R.string.confirm_remove_account)
+											.replace("{account}",
+													acc.getUser().getScreenName()))
+							.setPositiveButton(R.string.yes_str,
+									new DialogInterface.OnClickListener() {
+										@Override
+										public void onClick(DialogInterface dialog,
+												int which) {
+											dialog.dismiss();
+											AccountService.removeAccount(
+													getActivity(), acc);
+											getActivity().finish();
+										}
+									})
+							.setNegativeButton(R.string.no_str,
+									new DialogInterface.OnClickListener() {
+										@Override
+										public void onClick(DialogInterface dialog,
+												int which) {
+											dialog.dismiss();
+										}
+									});
+					ab.create().show();
+					return true;
+				}
+			});
+
+			SwitchPreference syncPref = ((SwitchPreference) findPreference(accountId
+					+ "_contactsync_on"));
+			syncPref.setChecked(ContentResolver.getSyncAutomatically(
+					AndroidAccountHelper.getAccount(getActivity(),
+							AccountService.getAccount(accountId)),
+					ContactsContract.AUTHORITY));
+			syncPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+				@Override
+				public boolean onPreferenceChange(final Preference preference,
+						Object newValue) {
+					AndroidAccountHelper.setServiceSync(ContactsContract.AUTHORITY, (Boolean)newValue,
+							AndroidAccountHelper.getAccount(getActivity(), AccountService.getAccount(accountId)));
+					return true;
+				}
+			});
+			
+			SwitchPreference c2dmPref = ((SwitchPreference) findPreference(accountId + "_c2dm"));
+			c2dmPref.setChecked(ContentResolver.getSyncAutomatically(
+					AndroidAccountHelper.getAccount(getActivity(),
+							AccountService.getAccount(accountId)),
+					NotificationService.AUTHORITY));
+			c2dmPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+				@Override
+				public boolean onPreferenceChange(
+						final Preference preference, Object newValue) {
+					
+					Log.d("boid", "notification switched");
+					AndroidAccountHelper.setServiceSync(NotificationService.AUTHORITY, (Boolean)newValue,
+							AndroidAccountHelper.getAccount(getActivity(), AccountService.getAccount(accountId)));
+					
+					return true;
+				}
+			});
+			
+			ListPreference c2dmPeriod = (ListPreference) findPreference(accountId + "_c2dm_period");
+			c2dmPeriod.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+				
+				@Override
+				public boolean onPreferenceChange(Preference preference, Object newValue) {
+					ContentResolver.addPeriodicSync(
+							AndroidAccountHelper.getAccount(getActivity(), AccountService.getAccount(accountId)), 
+							NotificationService.AUTHORITY,
+							new Bundle(),
+							Integer.parseInt((String)newValue) * 60);
+					preference.setSummary(getString(R.string.notification_every).replace("{x}", (String)newValue));
+					return true;
+				}
+			});
+			c2dmPeriod.setSummary(getString(R.string.notification_every).replace("{x}", c2dmPeriod.getValue() ));
+		}
+
+		void setKey(String key, long accountId) {
+			Preference p = findPreference("{user}_" + key);
+			if(p == null) return;
+			p.setKey(accountId + "_" + key);
 		}
 	}
 
---
+++ src/com/teamboid/twitter/views/AccountHeaderPreference.java
@@ -0,0 +1,36 @@
+package com.teamboid.twitter.views;
+
+import com.handlerexploit.prime.RemoteImageView;
+import com.teamboid.twitter.R;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.View;
+import me.kennydude.awesomeprefs.HeaderPreference;
+import me.kennydude.awesomeprefs.PreferenceFragment;
+
+public class AccountHeaderPreference extends HeaderPreference {
+
+	public AccountHeaderPreference(Context c, PreferenceFragment f) {
+		super(c, f);
+	}
+	
+	public String url;
+	
+	public View getIconView(){
+		RemoteImageView riv = new RemoteImageView(getContext());
+		
+		DisplayMetrics outMetrics = new DisplayMetrics();
+		getFragment().getActivity().getWindowManager().getDefaultDisplay().getMetrics(outMetrics);
+		float dp = outMetrics.density;
+		
+		riv.setMaxWidth((int) (64 * dp));
+		
+		riv.setImageResource(R.drawable.sillouette);
+		if(url != null){
+			riv.setImageURL(url);
+		}
+		return riv;
+	}
+
+}
